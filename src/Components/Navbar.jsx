f6b1e61c1ada58787079466c828d729d1cf1319e
djangid63 <jangiddevesh031@gmail.com>
2025-01-28 22:56:44 +0530

    initialize React app with Vite, Tailwind CSS, and basic components

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <section className='flex items-center justify-start text-sm'>
+      <div className='px-2'>Connections</div>
+      <div className='px-2'>Edit</div>
+      <div className='px-2'>View</div>
+      <div className='px-2'>Help</div>
+    </section>
+  )
+}
+
+export default navbar
