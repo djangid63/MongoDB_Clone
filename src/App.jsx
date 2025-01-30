0dcc632027407c01889435394aaea4280f5cac19
djangid63 <jangiddevesh031@gmail.com>
2025-01-30 11:31:54 +0530

    add Welcome page and integrate SideBar component; update color variables in CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from './Components/navbar'
 import SideBar from './Components/SideBar'
+import Welcome from './Pages/Welcome'
 
 const App = () => {
   return (
     <div className='overflow-x-hidden'>
       <Navbar />
-      <SideBar />
+      <Welcome />
     </div>
   )
 }
---
+++ src/Pages/Welcome.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SideBar from '../Components/SideBar'
+
+const Welcome = () => {
+  return (
+    <section className='grid grid-cols-[20vw_80vw]'>
+      <div>
+        <SideBar />
+      </div>
+      <div className='w-[80vw] h-[100%] bg-primary'></div>
+    </section>
+  )
+}
+
+export default Welcome
