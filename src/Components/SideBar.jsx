cc0a937299115f8a821c29de85060e4be636b5cc
djangid63 <jangiddevesh031@gmail.com>
2025-01-30 11:07:59 +0530

    enhance SideBar component with settings toggle and connection management

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -7,16 +7,27 @@ import { MdAdd } from "react-icons/md";
 import { RiFilter2Fill } from "react-icons/ri";
 import { GoCommandPalette } from "react-icons/go";
 import { TbDatabaseHeart } from "react-icons/tb";
+import { useState } from 'react';
 
 
 
 
 const SideBar = () => {
+  const [isSettingsOpen, setIsSettingsOpen] = useState(false);
+  const [isClusterExpanded, setIsClusterExpanded] = useState(false)
+  const [isConnected, setIsConnected] = useState(false);
+
+  const toggleSettings = () => setIsSettingsOpen(!isSettingsOpen);
+  const toggleCluster = () => setIsClusterExpanded(!isClusterExpanded);
+
   return (
     <section className='w-[20vw] h-[96vh] text-white justify-between items-start font-inter bg-secondary '>
       <div className='flex items-center justify-between pl-3'>
         <div className='text-xl py-2 font-bold'>Compass</div>
-        <span className='text-lg pr-6 pt-2'><IoIosSettings /></span>
+        <span className={`text-lg pr-6 pt-2 cursor-pointer ${isSettingsOpen ? 'text-blue-500' : ''}`}
+          onClick={toggleSettings}>
+          <IoIosSettings />
+        </span>
       </div>
       <div className='flex items-center justify-start mt-5 font-medium text-sm pl-3' >
         <span className=' pb-1'><PiBracketsCurlyBold /></span>
@@ -26,9 +37,11 @@ const SideBar = () => {
       <div className='flex items-center justify-between mt-5 font-medium text-sm pl-4' >
         <div>Connections</div>
         <div className='flex items-center justify-between mr-7 gap-4'>
-          <span><BiCollapseVertical /></span>
-          <span ><MdAdd size={18} /></span>
-          <span><BsThreeDots /></span>
+          <span className='hidden group-hover:block cursor-pointer'>
+            <BiCollapseVertical />
+          </span>
+          <span className='hidden group-hover:block cursor-pointer'><MdAdd size={18} /></span>
+          <span className='hidden group-hover:block cursor-pointer'><BsThreeDots /></span>
         </div>
       </div>
       {/* Search Bar */}
@@ -40,18 +53,20 @@ const SideBar = () => {
         />
         <span className="text-white text-xl p-2"><RiFilter2Fill /></span>
       </div>
-
-      <div className='flex justify-between pl-3 py-1 mt-3 text-sm bg-gray-600/40'>
+      {/* Cluster */}
+      <div className='flex justify-between pl-3 py-1 mt-3 text-sm hover:bg-gray-600/40 h-10 w-full group'>
         <div className='flex items-center'>
-          <span className='hidden '><IoMdArrowDropdown /></span>
-          <span className='text-xl'><IoMdArrowDropright /></span>
-          <span className=' '><TbDatabaseHeart /></span>
-          <div className='font-inter px-2  font-semibold'>cluster0.ygilo.mongodb.net</div>
+          <span className='cursor-pointer px-1' onClick={toggleCluster}>
+            {isClusterExpanded ? <IoMdArrowDropdown size={18} /> : <IoMdArrowDropright size={18} />}
+          </span>
+          <span className=''><TbDatabaseHeart /></span>
+          <div className='font-inter text-[13px] font-semibold mx-1'>cluster0.ygilo.mongod...</div>
         </div>
-        <div className='flex items-center justify-between gap-2'>
-          <span className='text-xs'><MdAdd size={18} /></span>
-          <span className='text-xs'><GoCommandPalette /></span>
-          <span className='text-xs mr-4'><BsThreeDots /> </span>
+        <div className={`flex items-center justify-between ${isConnected ? 'gap-3' : 'gap-0.5'}`}>
+          {!isConnected && <button onClick={() => setIsConnected(true)} className='hidden group-hover:block py-1 px-3 bg-gray-600/40 hover:bg-gray-400/60 border text-[12px] mx-2 border-gray-400 rounded-lg'>CONNECT</button>}
+          <span className={`text-xs cursor-pointer mr-1 hidden group-hover:block ${isConnected ? 'block' : 'hidden'}`}><MdAdd size={18} /></span>
+          <span className={`text-xs cursor-pointer hidden group-hover:block ${isConnected ? 'block' : 'hidden'}`}><GoCommandPalette /></span>
+          <span className='mr-4 hidden group-hover:block text-xs cursor-pointer'><BsThreeDots /></span>
         </div>
       </div>
     </section>
